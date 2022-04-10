bade1d06650055da335b58622b8a4b9fa3ff4ee8
Szamponetti <nowak.spiotr@gmail.com>
2022-04-10 12:54:11 +0200

    Initial setup of all dependencies and plugins

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default Home;
---
+++ styled.d.ts
@@ -0,0 +1,8 @@
+import "styled-components";
+
+type ThemeInterface = typeof theme;
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+  }
+}
